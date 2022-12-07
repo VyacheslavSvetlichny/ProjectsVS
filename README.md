0146a26bd5ce59e1a57e75667b0993b0cc5aa270
Vyacheslav Svetlichny <vyacheslavsvetlichny@gmail.com>
2022-12-07 11:47:07 +0700

    ecobag

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjectsVS
-My projects
---
+++ js/script.js
@@ -0,0 +1,28 @@
+var selector = document.querySelectorAll("input[type='tel']");
+var im = new Inputmask("+7 (999)-999-99-99");
+
+im.mask(selector);
+
+new Swiper('.catalog-slider',{
+
+    navigation: {
+        nextEl: '.catalog-next',
+        prevEl: '.catalog-prev'
+    },
+
+    slidesPerView: 5,
+    spaceBetween: 12,
+    loop: true,
+});
+
+new Swiper('.clients-slider',{
+
+    navigation: {
+        nextEl: '.clients-next',
+        prevEl: '.clients-prev'
+    },
+
+    slidesPerView: 6,
+    spaceBetween: 12,
+    loop: true,
+});
