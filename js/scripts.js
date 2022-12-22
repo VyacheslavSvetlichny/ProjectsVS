861177d67aa5bfecb93134aa1a7c3cf641286857
Vyacheslav Svetlichny <vyacheslavsvetlichny@gmail.com>
2022-12-22 17:28:54 +0700

    skill slider

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+new Swiper('.skill-hard',{
+
+    navigation: {
+        nextEl: '.skill-hard-next',
+        prevEl: '.skill-hard-prev'
+    },
+
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    // breakpoints: {
+    //   0: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 12
+    //   },
+    //   580: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 25
+    //   },
+    //   677: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 12
+    //   },
+    //   769: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 12
+    //       },
+    //   993: {
+    //     slidesPerView: 5,
+    //     spaceBetween: 12
+    //   },
+    //   1280: {
+    //     slidesPerView: 6,
+    //     spaceBetween: 12
+    //   }
+//   },
+});
